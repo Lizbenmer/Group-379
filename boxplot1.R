213992b079f4caf0ab80ee1abadccc65375f3908
Lizbenmer <lbenitezmerl@neiu.edu>
2025-10-26 21:46:21 +0000

    Here is my code boxplot1, hoping you can see it

---
+++ boxplot1.R
@@ -0,0 +1,21 @@
+#This 
+setwd("/cloud/project")
+
+TextMessages <- read.csv("TextMessages.csv", header=TRUE)
+TextMessages
+table(TextMessages$Group)
+
+install.packages("reshape")
+
+library(reshape)
+
+text <- melt(TextMessages, id=c("Participant","Group"), measured=c("Baseline","Six_months"))
+
+text$Group <- as.factor(text$Group)
+install.packages("ggplot2")
+
+library(ggplot2)
+
+ggplot(text,aes(x=Group,y=value)) + geom_boxplot()
+#NOTES: In Group 1, There are a couple of outliers. The data looks normally
+#distributed. My 25th percentile is about 53 a.....
